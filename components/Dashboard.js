f5d3fb590ef397570e59ad81db259ed7a8592ab2
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-07 16:15:30 +0700

    Initial commit

---
+++ components/Dashboard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import SpotifyWebApi from 'spotify-web-api-node';
+import Body from './Body'
+import Right from './Right'
+import Sidebar from './Sidebar'
+import { playingTrackState } from "../atoms/playerAtom";
+import { useRecoilState } from "recoil";
+import Player from './Player';
+import { useEffect, useState } from "react";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
+
+function Dashboard() {
+  const { data: session } = useSession();
+  const { accessToken } = session;
+
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
+  return (
+   <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
+     <Sidebar/>
+     <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack}/>
+     <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack}/>
+
+     {showPlayer && (
+        <div className="fixed bottom-0 left-0 right-0 z-50">
+          <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+        </div>
+      )}
+   </main>
+  )
+}
+
+export default Dashboard
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: [
+    // "./pages/index.js",
+    // "./pages/about.js",
+    // "./components/Body.js",
+    // "./components/Sidebar.js",
+    // "./components/Right.js",
+  ],
+  purge:["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: { screens: {
+      "2xl": "1500px",
+    },keyframes: {
+      pulse: {
+        "0%, 100%": {
+          opacity: 1,
+        },
+        "50%": {
+          opacity: 0.5,
+        },
+      },
+    },
+    animation: {
+      pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+    },
+  },
+    fontFamily: {
+      body: ["Andada Pro", "serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
+  },
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+  ],
+}
